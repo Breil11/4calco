a471cd01cce3fe5aca715e2db7c98442f35083d4
Breil <79250047+Breil11@users.noreply.github.com>
2025-01-15 15:11:00 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "calculator/cmd"
+// import "calculator/cmd"
+import "github.com/Breil11/4calco/super_calculator/cmd"
 
 func main() {
 	cmd.Execute()
