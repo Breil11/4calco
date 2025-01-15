e94245037e9068ca60cf7022a645222d536ef848
Breil <79250047+Breil11@users.noreply.github.com>
2025-01-15 15:24:52 +0100

    Create cmd.go

---
+++ super_calculator/cmd/cmd.go
@@ -0,0 +1,11 @@
+package cmd
+
+import (
+    "fmt"
+    // Importez ici vos autres fichiers si nécessaire, comme add.go, dic.go, etc.
+)
+
+func Execute() {
+    fmt.Println("Executing the 4calco commands!")
+    // Appelez ici des fonctions comme Add, Mul, etc., selon votre logique.
+}
